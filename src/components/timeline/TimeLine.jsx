c1860b353d4047bc90b07c7d7d85cb7903f7e8e9
ryuya0416 <67462677+ryuya416@users.noreply.github.com>
2022-05-25 11:29:24 +0900

    ✨ ダミーデータを用意

--- src/components/timeline/TimeLine.jsx
+++ src/components/timeline/TimeLine.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import { Post } from "../post/Post";
 import { Share } from "../share/Share";
 import "./TimeLine.css";
+import { Posts } from "../../dummyData";
 
 export const TimeLine = () => {
   return (
     <div className="timeline">
       <div className="timelineWrapper">
         <Share />
-        <Post />
+        {Posts.map((post) => (
+          <Post post={post} key={post.id} />
+        ))}
       </div>
     </div>
   );
