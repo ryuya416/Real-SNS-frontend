3c8b57220b118ce0dc60dc0337067a24a3e98336
ryuya0416 <67462677+ryuya416@users.noreply.github.com>
2022-05-25 10:56:51 +0900

    Rightbarコンポーネント✨

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -2,5 +2,61 @@ import React from "react";
 import "./Rightbar.css";
 
 export const Rightbar = () => {
-  return <div className="rightbar">Rightbar</div>;
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="eventConttainer">
+          <img src="assets/star.png" alt="" className="statImg" />
+          <span className="eventText">
+            <b>フォロワー限定</b>イベント開催中！
+          </span>
+        </div>
+        <img src="assets/ad.jpeg" alt="" className="eventImg" />
+        <h4 className="rightbarTitle">オンラインの友達</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="assets/person/1.jpeg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Shin Code</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="assets/person/2.jpeg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">tanaka Code</span>
+          </li>
+        </ul>
+        <p className="promotionTitle">プロモーション広告</p>
+        <img
+          src="assets/promotion/promotion1.jpeg"
+          alt=""
+          className="rightbarPromotionImg"
+        />
+        <p className="promotionName">ショッピング</p>
+        <img
+          src="assets/promotion/promotion2.jpeg"
+          alt=""
+          className="rightbarPromotionImg"
+        />
+        <p className="promotionName">カーショップ</p>
+        <img
+          src="assets/promotion/promotion3.jpeg"
+          alt=""
+          className="rightbarPromotionImg"
+        />
+        <p className="promotionName">shincode株式会社</p>
+      </div>
+    </div>
+  );
 };
