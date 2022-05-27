0d8f069fc63b933d6a2eff7f599c34e72627f665
ryuya0416 <67462677+ryuya416@users.noreply.github.com>
2022-05-27 20:17:24 +0900

    ✨

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,13 +1,22 @@
 import { MoreVert } from "@mui/icons-material";
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import "./Post.css";
-import { Users } from "../../dummyData";
 
 export const Post = ({ post }) => {
   const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
 
   const [like, setLike] = useState(post.like);
   const [isLiked, setIsLiked] = useState(false);
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const response = await axios.get(`/users/${post.userId}`);
+      setUser(response.data);
+    };
+    fetchUser();
+  }, []);
 
   const handleLike = () => {
     setLike(isLiked ? like - 1 : like + 1);
@@ -21,15 +30,12 @@ export const Post = ({ post }) => {
           <div className="postTopLeft">
             <img
               src={
-                PUBLIC_FOLDER +
-                Users.filter((user) => user.id === post.id)[0].profilePicture
+                user.profilePicture || PUBLIC_FOLDER + "/person/noAvatar.png"
               }
               alt=""
               className="postProfileImg"
             />
-            <span className="postUsername">
-              {Users.filter((user) => user.id === post.id)[0].username}
-            </span>
+            <span className="postUsername">{user.username}</span>
             <span className="postDate">{post.date}</span>
           </div>
           <div className="postTopRight">
